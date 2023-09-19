c18895035aee9174aba92153dd2198fd1cfc39b2
mshennessy <112807189+mshennessy@users.noreply.github.com>
2023-09-19 09:00:48 +0100

    Add files via upload

---
+++ magicskeleton.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+while (True):   # This will run forever
+    # uncomment and complete the next line
+    #question = input ()
+    randNum=randint(1,2)
+    if randNum == 1:
+        print("Outlook not so good")
+    else:
+        print("Definitely!")
+        
+
+print("Thanks for playing")
