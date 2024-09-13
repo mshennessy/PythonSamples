dd668925c1214afb86cb6c1db6593805bee16374
mshennessy <112807189+mshennessy@users.noreply.github.com>
2024-09-13 15:55:08 +0100

    Add files via upload

--- ClearScreenDynamicGrid_replit_only.py
+++ ClearScreenDynamicGrid_replit_only.py
@@ -1,27 +1,33 @@
-# G Hennessy CUS
+ G Hennessy CUS
 # Demo of how to give a dynamic display on replit using the replit clear() function
 # Note that this isn't available on Thonny, so the grids will just appear sequentially
 
 # Import the clear() function
-# Note that this won't work on Thonny even if you install replit module
 from replit import clear
 # Import the sleep function for a time delay so we can see the grid changing
 from time import sleep
 # Import random choice
 from random import randint
+# Allow output to have a background colour
+from colorama import Back
 
 fullGrid =[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
 
 def printGrid(grid):
   for i in range(len(grid)):
     for j in range(len(grid[i])):
-      print(grid[i][j], end="")
+      if grid[i][j] == 0:
+        print(Back.GREEN, grid[i][j],end= '')
+      else:
+        print(Back.RED, grid[i][j],end= '')
     print()
-
+    
+printGrid(fullGrid)
 for i in range(len(fullGrid)):
   for j in range(len(fullGrid[i])):
     # change each element in the row to a 0 or 1
     fullGrid[i][j]=randint(0,1)
   sleep(1)
   clear()
+  # Print the entire grid with updates
   printGrid(fullGrid)
