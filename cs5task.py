0fbd0b490e3378e165fa1738d7d5a54c8531e12b
mshennessy <112807189+mshennessy@users.noreply.github.com>
2025-11-27 13:48:08 +0000

    Add files via upload

---
+++ cs5task.py
@@ -0,0 +1,17 @@
+# Count how many vowels, consonants, spaces,
+
+vowels = ["a", "e", "i", "o", "u"]
+
+consonants = [
+    "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+    "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"
+]
+
+
+# Initialise a vowelCount and ConsCount
+
+# Ask the user to enter a word or sentence
+
+# Go through each character of the string entered
+# and count the vowels and the consonants
+# Tell the user how many vowels and consonants the text contained
